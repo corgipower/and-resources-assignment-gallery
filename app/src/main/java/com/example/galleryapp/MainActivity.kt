5f88706829f8f578137a497a4e50c8b4604e0710
corgipower <greensborodogtraining@gmail.com>
2019-08-12 23:30:47 -0400

    add strings for description

--- app/src/main/java/com/example/galleryapp/MainActivity.kt
+++ app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -17,31 +17,78 @@ class MainActivity : AppCompatActivity() {
         //create array of images, size of 10
         val size = 10
 
-        val imageViewsArray = MutableList(size){ ImageView(this) }
-        imageViewsArray[0].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.adventure_automobile_classic_2533092))
-        imageViewsArray[1].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.aerial_photography_aerial_shot_aerial_view_2583847))
-        imageViewsArray[2].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.alley_architecture_buildings_2526517))
-        imageViewsArray[3].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.architectural_design_architecture_bridge_2540653))
-        imageViewsArray[4].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.beautiful_breathtaking_canada_day_2526105))
+        val imageViewsArray = MutableList(size) { ImageView(this) }
+        imageViewsArray[0].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.adventure_automobile_classic_2533092
+            )
+        )
+        imageViewsArray[1].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.aerial_photography_aerial_shot_aerial_view_2583847
+            )
+        )
+        imageViewsArray[2].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.alley_architecture_buildings_2526517
+            )
+        )
+        imageViewsArray[3].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.architectural_design_architecture_bridge_2540653
+            )
+        )
+        imageViewsArray[4].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.beautiful_breathtaking_canada_day_2526105
+            )
+        )
         imageViewsArray[5].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.bloom_blossom_flora_2567011))
-        imageViewsArray[6].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.close_up_colorful_colors_2529148))
-        imageViewsArray[7].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.clouds_coconut_trees_daylight_2486168))
-        imageViewsArray[8].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.colorful_colourful_houses_2501965))
-        imageViewsArray[9].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.wallpaper_astronomy_astrophotography_2538107))
+        imageViewsArray[6].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.close_up_colorful_colors_2529148
+            )
+        )
+        imageViewsArray[7].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.clouds_coconut_trees_daylight_2486168
+            )
+        )
+        imageViewsArray[8].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.colorful_colourful_houses_2501965
+            )
+        )
+        imageViewsArray[9].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.wallpaper_astronomy_astrophotography_2538107
+            )
+        )
 
         //set up layout params
-        val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val layoutParams =
+            LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
 
         //Create an indexed forEach loop to step through the Views array
-        imageViewsArray.forEachIndexed { index, i -> i.layoutParams = layoutParams
-        i.setPadding(5, 5, 5, 5)
-        i.adjustViewBounds = true
-        if (index % 2 == 1) {
-        linear_layout_left.addView(i)}
-            else {
-            linear_layout_right.addView(i)
+        imageViewsArray.forEachIndexed { index, i ->
+            i.layoutParams = layoutParams
+            i.setPadding(5, 5, 5, 5)
+            i.adjustViewBounds = true
+            if (index % 2 == 1) {
+                linear_layout_left.addView(i)
+            } else {
+                linear_layout_right.addView(i)
+            }
         }
-       }
 
     }
 }
